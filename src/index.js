51e923d5e022d556e04176c61806a8fde25bac5c
upisfree <upisfree@outlook.com>
2017-10-25 00:43:28 +0600

    remove unnecessary xhr

--- src/index.js
+++ src/index.js
@@ -7,24 +7,12 @@ var Az = require('./az'),
 // массив со всеми названиями и ключевыми словами, с которым сначала идёт проверка, мол, стоит ли уже дальше проверять все эмоджи или нет
 // fallback: https://github.com/lautis/emojie
 
-var input = document.getElementById('input');
-var output = document.getElementById('output');
-
-document.getElementsByTagName('button')[0].onclick = function() {
-  output.textContent = translateText(input.value);
-}
-
-var xhr = new XMLHttpRequest();
-
-xhr.open('GET', 'converter/emojies.json', true);
-xhr.onreadystatechange = function() {
-  if (xhr.readyState != 4)
-    return;
-
-  window.emojiData = JSON.parse(xhr.responseText);
-};
-
 // init
 Az.Morph.init('node_modules/az/dicts', function() {
-  xhr.send();
+  var input = document.getElementById('input');
+  var output = document.getElementById('output');
+
+  document.getElementsByTagName('button')[0].onclick = function() {
+    output.textContent = translateText(input.value);
+  };
 });
--- src/translateText.js
+++ src/translateText.js
@@ -1,5 +1,6 @@
 var Az = require('./az'),
     LANG = require('./lang'),
+    emojies = require('../converter/emojies.json'),
     prepareText = require('./prepareText'),
     tokenization = require('./tokenization'),
     normalizeWord = require('./normalizeWord'),
@@ -27,7 +28,7 @@ module.exports = function(text) {
       word = normalizeWord(tokens[a].toString(), lang);
 
       if (word) { // отсекаем не существительные, глаголы или прилагательные
-        let emoji = wordToEmoji(word, lang, emojiData);
+        let emoji = wordToEmoji(word, lang, emojies);
 
         if (emoji) {
           tokens[a] += ' ' + emoji;
