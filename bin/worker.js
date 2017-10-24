7d5d3d2fe837d2644051d2fa490c436ed843044a
upisfree <upisfree@outlook.com>
2017-10-25 02:49:48 +0600

    add word normalization for english by Porter algorithm

--- bin/worker.js
+++ bin/worker.js
@@ -2479,6 +2479,204 @@ module.exports={"♀":{"ru":{"keywords":["женщина","символ","вен
 },{"fs":3}],3:[function(require,module,exports){
 
 },{}],4:[function(require,module,exports){
+'use strict';
+
+module.exports = stemmer;
+
+/* Character code for `y`. */
+var CC_Y = 'y'.charCodeAt(0);
+
+/* Standard suffix manipulations. */
+
+var step2list = {
+  ational: 'ate',
+  tional: 'tion',
+  enci: 'ence',
+  anci: 'ance',
+  izer: 'ize',
+  bli: 'ble',
+  alli: 'al',
+  entli: 'ent',
+  eli: 'e',
+  ousli: 'ous',
+  ization: 'ize',
+  ation: 'ate',
+  ator: 'ate',
+  alism: 'al',
+  iveness: 'ive',
+  fulness: 'ful',
+  ousness: 'ous',
+  aliti: 'al',
+  iviti: 'ive',
+  biliti: 'ble',
+  logi: 'log'
+};
+
+var step3list = {
+  icate: 'ic',
+  ative: '',
+  alize: 'al',
+  iciti: 'ic',
+  ical: 'ic',
+  ful: '',
+  ness: ''
+};
+
+/* Consonant-vowel sequences. */
+
+var consonant = '[^aeiou]';
+var vowel = '[aeiouy]';
+var consonantSequence = '(' + consonant + '[^aeiouy]*)';
+var vowelSequence = '(' + vowel + '[aeiou]*)';
+
+var MEASURE_GT_0 = new RegExp(
+  '^' + consonantSequence + '?' + vowelSequence + consonantSequence
+);
+
+var MEASURE_EQ_1 = new RegExp(
+  '^' + consonantSequence + '?' + vowelSequence + consonantSequence +
+  vowelSequence + '?$'
+);
+
+var MEASURE_GT_1 = new RegExp(
+  '^' + consonantSequence + '?' +
+  '(' + vowelSequence + consonantSequence + '){2,}'
+);
+
+var VOWEL_IN_STEM = new RegExp(
+  '^' + consonantSequence + '?' + vowel
+);
+
+var CONSONANT_LIKE = new RegExp(
+  '^' + consonantSequence + vowel + '[^aeiouwxy]$'
+);
+
+/* Exception expressions. */
+
+var SUFFIX_LL = /ll$/;
+var SUFFIX_E = /^(.+?)e$/;
+var SUFFIX_Y = /^(.+?)y$/;
+var SUFFIX_ION = /^(.+?(s|t))(ion)$/;
+var SUFFIX_ED_OR_ING = /^(.+?)(ed|ing)$/;
+var SUFFIX_AT_OR_BL_OR_IZ = /(at|bl|iz)$/;
+var SUFFIX_EED = /^(.+?)eed$/;
+var SUFFIX_S = /^.+?[^s]s$/;
+var SUFFIX_SSES_OR_IES = /^.+?(ss|i)es$/;
+var SUFFIX_MULTI_CONSONANT_LIKE = /([^aeiouylsz])\1$/;
+var STEP_2 = new RegExp(
+  '^(.+?)(ational|tional|enci|anci|izer|bli|alli|entli|eli|ousli|' +
+  'ization|ation|ator|alism|iveness|fulness|ousness|aliti|iviti|' +
+  'biliti|logi)$'
+);
+var STEP_3 = /^(.+?)(icate|ative|alize|iciti|ical|ful|ness)$/;
+var STEP_4 = new RegExp(
+  '^(.+?)(al|ance|ence|er|ic|able|ible|ant|ement|ment|ent|ou|ism|ate|' +
+  'iti|ous|ive|ize)$'
+);
+
+/* Stem `value`. */
+function stemmer(value) {
+  var firstCharacterWasLowerCaseY;
+  var match;
+
+  value = String(value).toLowerCase();
+
+  /* Exit early. */
+  if (value.length < 3) {
+    return value;
+  }
+
+  /* Detect initial `y`, make sure it never
+   * matches. */
+  if (value.charCodeAt(0) === CC_Y) {
+    firstCharacterWasLowerCaseY = true;
+    value = 'Y' + value.substr(1);
+  }
+
+  /* Step 1a. */
+  if (SUFFIX_SSES_OR_IES.test(value)) {
+    /* Remove last two characters. */
+    value = value.substr(0, value.length - 2);
+  } else if (SUFFIX_S.test(value)) {
+    /* Remove last character. */
+    value = value.substr(0, value.length - 1);
+  }
+
+  /* Step 1b. */
+  if (match = SUFFIX_EED.exec(value)) {
+    if (MEASURE_GT_0.test(match[1])) {
+      /* Remove last character. */
+      value = value.substr(0, value.length - 1);
+    }
+  } else if (
+    (match = SUFFIX_ED_OR_ING.exec(value)) &&
+    VOWEL_IN_STEM.test(match[1])
+  ) {
+    value = match[1];
+
+    if (SUFFIX_AT_OR_BL_OR_IZ.test(value)) {
+      /* Append `e`. */
+      value += 'e';
+    } else if (
+      SUFFIX_MULTI_CONSONANT_LIKE.test(value)
+    ) {
+      /* Remove last character. */
+      value = value.substr(0, value.length - 1);
+    } else if (CONSONANT_LIKE.test(value)) {
+      /* Append `e`. */
+      value += 'e';
+    }
+  }
+
+  /* Step 1c. */
+  if ((match = SUFFIX_Y.exec(value)) && VOWEL_IN_STEM.test(match[1])) {
+    /* Remove suffixing `y` and append `i`. */
+    value = match[1] + 'i';
+  }
+
+  /* Step 2. */
+  if ((match = STEP_2.exec(value)) && MEASURE_GT_0.test(match[1])) {
+    value = match[1] + step2list[match[2]];
+  }
+
+  /* Step 3. */
+  if ((match = STEP_3.exec(value)) && MEASURE_GT_0.test(match[1])) {
+    value = match[1] + step3list[match[2]];
+  }
+
+  /* Step 4. */
+  if (match = STEP_4.exec(value)) {
+    if (MEASURE_GT_1.test(match[1])) {
+      value = match[1];
+    }
+  } else if ((match = SUFFIX_ION.exec(value)) && MEASURE_GT_1.test(match[1])) {
+    value = match[1];
+  }
+
+  /* Step 5. */
+  if (
+    (match = SUFFIX_E.exec(value)) &&
+    (
+      MEASURE_GT_1.test(match[1]) ||
+      (MEASURE_EQ_1.test(match[1]) && !CONSONANT_LIKE.test(match[1]))
+    )
+  ) {
+    value = match[1];
+  }
+
+  if (SUFFIX_LL.test(value) && MEASURE_GT_1.test(value)) {
+    value = value.substr(0, value.length - 1);
+  }
+
+  /* Turn initial `Y` back to `y`. */
+  if (firstCharacterWasLowerCaseY) {
+    value = 'y' + value.substr(1);
+  }
+
+  return value;
+}
+
+},{}],5:[function(require,module,exports){
 var Az = require('az');
 
 // fix Az lib
@@ -2497,15 +2695,16 @@ Az.load = function(url, responseType, callback) {
 }
 
 module.exports = Az
-},{"az":2}],5:[function(require,module,exports){
+},{"az":2}],6:[function(require,module,exports){
 const LANG = {
   RU: 'ru',
   EN: 'en'
 }
 
 module.exports = LANG;
-},{}],6:[function(require,module,exports){
-var Az = require('./az')
+},{}],7:[function(require,module,exports){
+var Az = require('./az'),
+    stemmer = require('stemmer'),
     LANG = require('./lang');
 
 module.exports = function(word, lang) {
@@ -2513,38 +2712,44 @@ module.exports = function(word, lang) {
     case LANG.RU:
       word = Az.Morph(word)[0];
 
-      // приводим в начальную форму только существительные, глаголы и прилагательные
-      if (word.tag.POS === 'NOUN' ||
-          // word.tag.POS === 'ADVB' || // наречие
-          word.tag.POS === 'INFN' ||
-          word.tag.POS === 'ADJF') {
-        word = word.normalize().word; // в русском только здесь эмоджи подставляем
-      } else {
-        word = null;
+      if (word) { // случается, что предсказыватель ничего не предсказывает
+        // приводим в начальную форму только существительные, глаголы и прилагательные
+        if (word.tag.POS === 'NOUN' ||
+            // word.tag.POS === 'ADVB' || // наречие
+            word.tag.POS === 'INFN' ||
+            word.tag.POS === 'ADJF') {
+          word = word.normalize().word; // в русском только здесь эмоджи подставляем
+        } else {
+          word = null;
+        }
       }
 
       break;
     case LANG.EN:
-      word = word;
+      if (word !== 'on' && word !== 'a' && word !== 'it' && word !== 'is') {
+        word = stemmer(word);
+      } else {
+        word = null;
+      }
 
       break;
   }
 
   return word;
 }
-},{"./az":4,"./lang":5}],7:[function(require,module,exports){
+},{"./az":5,"./lang":6,"stemmer":4}],8:[function(require,module,exports){
 module.exports = function(text) {
   var specialSymbols = /(@|&|'|\(|\)|<|>|#)/g;
 
   return text.replace(specialSymbols, '').toLowerCase();
 }
-},{}],8:[function(require,module,exports){
+},{}],9:[function(require,module,exports){
 var Az = require('./az');
 
 module.exports = function(text) {
   return Az.Tokens(text).done();
 }
-},{"./az":4}],9:[function(require,module,exports){
+},{"./az":5}],10:[function(require,module,exports){
 var Az = require('./az'),
     LANG = require('./lang'),
     emojies = require('../converter/emojies.json'),
@@ -2586,7 +2791,7 @@ module.exports = function(text) {
 
   return tokens.join(' ');
 }
-},{"../converter/emojies.json":1,"./az":4,"./lang":5,"./normalizeWord":6,"./prepareText":7,"./tokenization":8,"./wordToEmoji":10}],10:[function(require,module,exports){
+},{"../converter/emojies.json":1,"./az":5,"./lang":6,"./normalizeWord":7,"./prepareText":8,"./tokenization":9,"./wordToEmoji":11}],11:[function(require,module,exports){
 module.exports = function(word, lang, emojies) {
   let variants = [];
 
@@ -2611,7 +2816,7 @@ module.exports = function(word, lang, emojies) {
 
   return variants[Math.floor(Math.random() * variants.length)];
 }
-},{}],11:[function(require,module,exports){
+},{}],12:[function(require,module,exports){
 var Az = require('./az'),
     translateText = require('./translateText');
 
@@ -2620,4 +2825,4 @@ Az.Morph.init('../node_modules/az/dicts', function() {
     postMessage(translateText(e.data));
   }
 });
-},{"./az":4,"./translateText":9}]},{},[11]);
+},{"./az":5,"./translateText":10}]},{},[12]);
--- src/normalizeWord.js
+++ src/normalizeWord.js
@@ -1,4 +1,5 @@
-var Az = require('./az')
+var Az = require('./az'),
+    stemmer = require('stemmer'),
     LANG = require('./lang');
 
 module.exports = function(word, lang) {
@@ -6,19 +7,25 @@ module.exports = function(word, lang) {
     case LANG.RU:
       word = Az.Morph(word)[0];
 
-      // приводим в начальную форму только существительные, глаголы и прилагательные
-      if (word.tag.POS === 'NOUN' ||
-          // word.tag.POS === 'ADVB' || // наречие
-          word.tag.POS === 'INFN' ||
-          word.tag.POS === 'ADJF') {
-        word = word.normalize().word; // в русском только здесь эмоджи подставляем
-      } else {
-        word = null;
+      if (word) { // случается, что предсказыватель ничего не предсказывает
+        // приводим в начальную форму только существительные, глаголы и прилагательные
+        if (word.tag.POS === 'NOUN' ||
+            // word.tag.POS === 'ADVB' || // наречие
+            word.tag.POS === 'INFN' ||
+            word.tag.POS === 'ADJF') {
+          word = word.normalize().word; // в русском только здесь эмоджи подставляем
+        } else {
+          word = null;
+        }
       }
 
       break;
     case LANG.EN:
-      word = word;
+      if (word !== 'on' && word !== 'a' && word !== 'it' && word !== 'is') {
+        word = stemmer(word);
+      } else {
+        word = null;
+      }
 
       break;
   }
