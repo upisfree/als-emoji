2aa93c6a1540ef5804c366d32d62ac4d775ee91a
upisfree <upisfree@outlook.com>
2017-10-31 22:29:34 +0600

    add lol kek cheburek

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  LONG_TEXT_LENGTH: 6000, // when text becomes long
+  LONG_TEXT_LENGTH: 6000, // when text is long?
   LONG_TEXT_DELAY: 300 // set 0 for instant translate. it's delay for preventing thing like this: https://i.imgur.com/6ZChXob.gif
 };
 
