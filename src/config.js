cd6291334e41ac4cdbc82707974c2ae548f118fd
upisfree <upisfree@outlook.com>
2017-10-31 20:29:14 +0600

    add delay after typing stops

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  TYPING_END_DELAY: 300 // set 0 for instant translate. it's delay for preventing thing like this: https://i.imgur.com/6ZChXob.gif
+};
+
+module.exports = CONFIG;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
 // лол кек фу и прочее добавить !
-// ждать, пока юзер кончит печатать и тогда отправлять воркеру, а не по изменению текста (но и учитывать ctrl+c, ctrl+v)
 // гульпом при релизе копировать json и делать из него js и подключать в html. эмоджи вынести в отдельный файл, что коннектится в index.html, чтобы можно было использовать эмоджи в UI (название тулзы: радость —> 😂, когда наводишь, делает это очень быстро)
 // пора начать оформлять дизайн
 // на старых виндах fallback: https://github.com/lautis/emojie
 // (?) diff таки надо, время последней отправки 
 //
+var CONFIG = require('./config'),
+    time = Date.now(),
+    input = document.getElementById('input'),
+    output = document.getElementById('output'),
+    worker = new Worker('./bin/worker.js');
 
+function tick() {
+  if ((Date.now() - time) > CONFIG.TYPING_END_DELAY && time !== 0) {
+    time = 0;
 
-var input = document.getElementById('input');
-var output = document.getElementById('output');
+    worker.postMessage(input.value);
+  }
 
-var worker = new Worker('./bin/worker.js');
+  requestAnimationFrame(tick);
+}
 
 worker.onmessage = function(e) {
   output.textContent = e.data;
 }
 
-input.oninput = function() {
-  worker.postMessage(input.value);
+input.oninput = input.onchange = function() {
+  time = Date.now();
 }
 
-// requestAnimationFrame
+requestAnimationFrame(tick);
